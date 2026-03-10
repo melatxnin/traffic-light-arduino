f0c3337c79937dd7b0f6e3f31474ca45f7591fa8
Melatxnin <nicolas.gossard@icloud.com>
2026-03-10 19:31:01 +0400

    Initial traffic light simulation

---
+++ sketch.ino
@@ -0,0 +1,41 @@
+/*
+  ROULETTE GAME
+
+  Author:
+  Nicolas GOSSARD
+*/
+
+// pins
+constexpr int redPin = 9;
+constexpr int orangePin = 8;
+constexpr int greenPin = 7;
+
+// time
+constexpr int redTime = 4;
+constexpr int orangeTime = 1;
+constexpr int greenTime = 3;
+
+void setup()
+{
+  pinMode(redPin, OUTPUT);
+  pinMode(orangePin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(greenPin, HIGH);
+  digitalWrite(orangePin, LOW);
+  digitalWrite(redPin, LOW);
+  delay(greenTime);
+
+  digitalWrite(greenPin, LOW);
+  digitalWrite(orangePin, HIGH);
+  digitalWrite(redPin, LOW);
+  delay(orangeTime);
+
+  digitalWrite(greenPin, LOW);
+  digitalWrite(orangePin, LOW);
+  digitalWrite(redPin, HIGH);
+  delay(redTime);
+}
